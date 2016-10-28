673a9f7006bfe25c638d668306c937831866e457
Zarnosch <janoszwei@yahoo.de>
2016-10-29 01:18:36 +0200

    main menu

---
+++ DieDungeon/Assets/Scripts/Behaviour/MainMenu/MainMenuBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+
+    public void OnGameStart()
+    {
+        //Application.LoadLevel();
+    }
+
+    public void OnGameQuit()
+    {
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
