9f6f3979177220392de7b0ff2e1b94a9604e7696
YsenGrimm <kloeten.wars@googlemail.com>
2016-10-29 17:07:29 +0200

    camera movement

---
+++ DieDungeon/Assets/Scripts/Behaviour/CameraBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public enum ViewDirection {
+		LEFT,
+		RIGHT
+	}
+
+	// public
+	public float border = 1.5f;
+	public float cameraChangeSpeed = 0.2f;
+
+	public bool drawDebug = false;
+
+	[System.NonSerialized]
+	public ViewDirection viewDirection;
+
+	// private
+	private bool viewChanged;
+	private float datRunningVar = 0;
+	private ViewDirection oldViewDirection = ViewDirection.RIGHT;
+
+	private Camera cam;
+
+	void Awake() {
+		cam = Camera.main;
+	}
+
+	void Update() {
+
+		if (drawDebug) {
+			Debug.DrawLine (new Vector3 (cam.transform.position.x - border, transform.position.y + 2, transform.position.z),
+				new Vector3 (cam.transform.position.x - border, transform.position.y - 2, transform.position.z));
+
+			Debug.DrawLine (new Vector3 (cam.transform.position.x + border, transform.position.y + 2, transform.position.z),
+				new Vector3 (cam.transform.position.x + border, transform.position.y - 2, transform.position.z));	
+		}
+			
+		if (oldViewDirection != viewDirection) {
+			datRunningVar = 0;
+			viewChanged = true;
+			oldViewDirection = viewDirection;
+		}
+
+		float lerpedCameraX = 0;
+
+		if (viewChanged && viewDirection == ViewDirection.LEFT) {
+			datRunningVar += cameraChangeSpeed * Time.deltaTime;
+			lerpedCameraX = Mathf.Lerp (cam.transform.position.x, transform.position.x + border, datRunningVar);
+
+			if (datRunningVar >= 1) {
+				datRunningVar = 0;
+				viewChanged = false;
+			}
+		} else if (viewChanged && viewDirection == ViewDirection.RIGHT) {
+			datRunningVar += cameraChangeSpeed * Time.deltaTime;
+			lerpedCameraX = Mathf.Lerp (cam.transform.position.x, transform.position.x - border, datRunningVar);
+
+			if (datRunningVar >= 1) {
+				datRunningVar = 0;
+				viewChanged = false;
+			}
+		} else {
+			if (viewDirection == ViewDirection.LEFT) {
+				lerpedCameraX = transform.position.x + border;
+			} else {
+				lerpedCameraX = transform.position.x - border;
+			}
+		}
+
+		oldViewDirection = viewDirection;
+			
+		cam.transform.position = new Vector3 (lerpedCameraX, transform.position.y, cam.transform.position.z);
+	}
+}
--- DieDungeon/Assets/Scripts/Behaviour/Player/PlayerMovementBehaviour.cs
+++ DieDungeon/Assets/Scripts/Behaviour/Player/PlayerMovementBehaviour.cs
@@ -18,17 +18,25 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 	private PlayerJumpBehaviour pj;
 	private Rigidbody2D rb;
 	private ActiveInTimeLayerBehaviour ait;
+	private CameraBehaviour cb;
 
 	void Awake() {
 		pj = GetComponent<PlayerJumpBehaviour>();
 		rb = GetComponent<Rigidbody2D>();
 		ait = GetComponent<ActiveInTimeLayerBehaviour>();
+		cb = GetComponent<CameraBehaviour>();
 	}
 
 	void Update() {
 		if (IngameHandlerBehaviour.Instance.Handler.ActiveTimeLayer != ait.ActiveInTimeLayer) { return; }
 
 		float input = Input.GetAxisRaw("Horizontal");
+
+		if (input > 0) {
+			cb.viewDirection = CameraBehaviour.ViewDirection.LEFT;
+		} else if (input < 0){
+			cb.viewDirection = CameraBehaviour.ViewDirection.RIGHT;
+		}
 
 		if (input != 0) {
 			velocity += accelerationSpeed * Time.deltaTime;
