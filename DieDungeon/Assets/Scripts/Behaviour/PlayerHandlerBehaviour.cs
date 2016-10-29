fbea85b7f65aa91bc246dc6f1b7c37934b4f35de
YsenGrimm <kloeten.wars@googlemail.com>
2016-10-29 17:44:49 +0200

    better inception spawning

--- DieDungeon/Assets/Scripts/Behaviour/PlayerHandlerBehaviour.cs
+++ DieDungeon/Assets/Scripts/Behaviour/PlayerHandlerBehaviour.cs
@@ -6,6 +6,9 @@ public class PlayerHandlerBehaviour : SingletonBehaviour<PlayerHandlerBehaviour>
 	// public
 	public PlayerBehaviour playerPrefab;
 
+	public Vector2 spawnPosition = new Vector2(0, 4);
+
+	[HideInInspector]
 	public PlayerBehaviour activePlayer;
 
 	// private
@@ -20,7 +23,12 @@ public class PlayerHandlerBehaviour : SingletonBehaviour<PlayerHandlerBehaviour>
 	public void CreatePlayer() {
 		PlayerBehaviour player = GameObject.Instantiate(playerPrefab) as PlayerBehaviour;
 		player.transform.parent = transform;
+		player.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, 0);
+
+		player.gameObject.SetActive(true);
+
 		player.GetComponent<ActiveInTimeLayerBehaviour>().ActiveInTimeLayer = IngameHandlerBehaviour.Instance.Handler.ActiveTimeLayer;
+
 		players.Add(player);
 
 		// set newly created player as active player
