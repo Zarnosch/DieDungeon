88ff914ebafed86917f2587d42d832de521404b9
wookiepeter <matthis.hagen@o2online.de>
2016-10-29 17:25:12 +0200

    added in healthbar and overwrote prefab

--- DieDungeon/Assets/Scripts/Behaviour/Enemie/EnemieBehaviour.cs
+++ DieDungeon/Assets/Scripts/Behaviour/Enemie/EnemieBehaviour.cs
@@ -5,12 +5,24 @@ using System.Collections;
 public class EnemieBehaviour : MonoBehaviour
 {
     public EnemieData Data;
+    private HealthBar bar;
+
+    public void Awake()
+    {
+        bar = gameObject.GetComponentInChildren<HealthBar>();
+        Data.Start();
+    }
+
+    public void Start()
+    {
+        Data.Update(bar);
+    }
 
     public void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.GetComponent<RangedWeaponBehaviour>() != null && coll.gameObject.GetComponent<OwnedByBehaviour>().Owner != Owner.Enemie)
         {
-            if (!Data.TakeDamage(coll.gameObject.GetComponent<RangedWeaponBehaviour>().Data.Damage))
+            if (!Data.TakeDamage(coll.gameObject.GetComponent<RangedWeaponBehaviour>().Data.Damage, bar))
                 KillMySelf();
         }
     }
