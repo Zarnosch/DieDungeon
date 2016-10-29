4709fa899d3cad73d9766abdee70d9dcff45013c
YsenGrimm <kloeten.wars@googlemail.com>
2016-10-29 02:38:59 +0200

    player behaviour

---
+++ DieDungeon/Assets/Scripts/Behaviour/Player/PlayerBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	// public
+	public Vector2 spawnPos = new Vector2(0, 0);
+
+	// private
+	private PlayerData player;
+
+	void Awake() {
+		player = new PlayerData(spawnPos);
+	}
+
+}
